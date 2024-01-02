1c014ac309d02554a61b5c6b7da2269eee23cf82
QSP <QSP@DESKTOP-BKE9C99>
2024-01-02 14:31:33 +0530

    done

---
+++ simple/src/test/java/Recived/Recive.java
@@ -0,0 +1,13 @@
+package Recived;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Recive {
+	@Test
+	public void demo()
+	{
+		Reporter.log("hello",true);
+	}
+
+}
